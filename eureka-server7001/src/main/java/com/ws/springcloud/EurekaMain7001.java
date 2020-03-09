9e0dab5bed1a66534b0992ff29673abd3e1fc310
lidagen <wal365@126.com>
2020-03-09 19:58:59 +0800

    新增7001模块

--- eureka-server7001/src/main/java/com/ws/springcloud/EurekaMain7001.java
+++ eureka-server7001/src/main/java/com/ws/springcloud/EurekaMain7001.java
@@ -2,13 +2,15 @@ package com.ws.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * USER: wang_s
  * DATE：2020/3/9
- * DESC：
+ * DESC：Eureka服务注册中心
  **/
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaMain7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class, args);
