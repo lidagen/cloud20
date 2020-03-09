36042d34d0b9d50e5fad4731a53b122ae9285a70
lidagen <wal365@126.com>
2020-03-09 12:10:29 +0800

    改造Aop

--- api/src/main/java/com/ws/springcloud/aop/MyAop.java
+++ api/src/main/java/com/ws/springcloud/aop/MyAop.java
@@ -1,8 +1,14 @@
 package com.ws.springcloud.aop;
 
+import com.ws.springcloud.common.result.CommonResult;
+import com.ws.springcloud.exc.AppException;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,16 +18,55 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 @Aspect
-
+@Slf4j
 public class MyAop {
 
     /**
      * 定义切入点，切入点为  opController中的所有函数
      *通过@Pointcut注解声明频繁使用的切点表达式
      */
-    /*@Pointcut("execution(public * com.ws.springcloud.common.result.CommonResult*(..)))")
+    @Pointcut("execution(public com.ws.springcloud.common.result.CommonResult *(..))")
     public void MyAop(){
 
-    }*/
+    }
+
+    @Around("MyAop()")
+    public CommonResult<?> processAuthority(ProceedingJoinPoint pjp) throws Throwable{
+        long startTime = System.currentTimeMillis();
+
+        CommonResult<?> result;
+
+        try {
+            result = (CommonResult<?>) pjp.proceed();
+            log.info(pjp.getSignature() + "use time:" + (System.currentTimeMillis() - startTime));
+        } catch (Throwable e) {
+            result = handlerException(pjp, e);
+        }
+
+        return result;
+    }
+
+    /**
+     * 封装异常信息，注意区分已知异常（自己抛出的）和未知异常
+     */
+    private CommonResult<?> handlerException(ProceedingJoinPoint pjp, Throwable e) {
+        CommonResult<?> result = new CommonResult();
+
+        // 已知异常
+        if (e instanceof AppException) {
+            result.setMessage(e.getLocalizedMessage());
+            result.setCode(401);/*
+        } else if (e instanceof UnloginException) {
+            result.setMsg("Unlogin");
+            result.setCode(ResultBean.NO_LOGIN);*/
+        } else {
+            log.error(pjp.getSignature() + " error ", e);
+            //TODO 未知的异常，应该格外注意，可以发送邮件通知等
+            result.setMessage(e.toString());
+            result.setCode(400);
+        }
+
+        return result;
+    }
 
 }
