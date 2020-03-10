6884c4f544cf2f1a5d98e529f773c8aa274790fd
lidagen <wal365@126.com>
2020-03-10 11:14:18 +0800

    eureka集群

---
+++ eureka-server7002/src/main/java/com/ws/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.ws.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * USER: wang_s
+ * DATE：2020/3/10
+ * DESC：
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
