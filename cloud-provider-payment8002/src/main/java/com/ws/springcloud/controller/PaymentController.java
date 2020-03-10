4b878e993212e65cb5a5d6f33781afdadbe14668
lidagen <wal365@126.com>
2020-03-10 16:13:33 +0800

    注册发现commit

--- cloud-provider-payment8002/src/main/java/com/ws/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/ws/springcloud/controller/PaymentController.java
@@ -3,8 +3,13 @@ package com.ws.springcloud.controller;
 import com.ws.springcloud.common.result.CommonResult;
 import com.ws.springcloud.entites.payment.Payment;
 import com.ws.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * USER: wang_s
@@ -13,9 +18,13 @@ import org.springframework.web.bind.annotation.*;
  **/
 @RestController
 @RequestMapping("/payment")
+@Slf4j
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -27,5 +36,15 @@ public class PaymentController {
     public CommonResult<Payment> getById(@PathVariable("id") Long id){
 
         return new CommonResult<>(paymentService.getById(id));
+    }
+
+    @GetMapping(value = "/discovery")
+    public CommonResult<Object> discoveryClient(){
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("======>"+instance.getInstanceId()+"\t"+instance.getServiceId()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return new CommonResult<>(services);
     }
 }
